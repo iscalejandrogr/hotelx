aa36368eb0428c5dd74862faf4770e19684036e5
Alejandro Guzmán <i.s.c.alejandro.gr@gmail.com>
2013-05-05 22:41:46 -0500

    nuevox
    nuevox

---
+++ js/API/archivos.js
@@ -0,0 +1,17 @@
+//archivos
+function uploadPhoto(imageURI) {
+            var options = new FileUploadOptions();
+            options.fileKey="file";
+            options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+            options.mimeType="image/jpeg";
+
+            var params = {};
+            params.value1 = "test";
+            params.value2 = "param";
+
+            options.params = params;
+
+            var ft = new FileTransfer();
+            ft.upload(imageURI, encodeURI("http://some.server.com/upload.php"), win, fail, options);
+        }
+
